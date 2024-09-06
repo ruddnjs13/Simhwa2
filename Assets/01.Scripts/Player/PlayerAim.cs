ed40d36ec73bcbd591f352428a06d5f9d4656501
ruddnjs13 <ruddnjs013@gmail.com>
2024-09-07 00:47:48 +0900

    PlayerAim&Enemy

--- Assets/01.Scripts/Player/PlayerAim.cs
+++ Assets/01.Scripts/Player/PlayerAim.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerAim : MonoBehaviour, IPlayerComponent
 {
@@ -12,15 +13,59 @@ public class PlayerAim : MonoBehaviour, IPlayerComponent
 
     [Header("SightInfo")] [Range(0f, 360f)]
     public float viewAngle;
-
     [Range(1f, 12f)] public float viewRadius;
-
     public Vector3 HolderPosition => _weaponHolder.position;
+
+    [Header("RayCastInfo")] 
+    [SerializeField] private ContactFilter2D _enemyFilter;
+
+    [SerializeField] private LayerMask _obstacleMask;
+    [SerializeField] private float _enemyFindDelay = 0.3f;
+    [SerializeField] private int _maxCheckCount = 10;
+     public List<Transform> visibleTargets = new List<Transform>();
+     private Collider2D[] _enemiesInView;
 
     public void Initialize(Player player)
     {
         _player = player;
         _inputReader = _player.GetCompo<InputReaderSO>();
+        _enemiesInView = new Collider2D[_maxCheckCount];
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FindEnemyWithDelay());
+    }
+
+    private IEnumerator FindEnemyWithDelay()
+    {
+        var time = new WaitForSeconds(_enemyFindDelay);
+        while (true)
+        {
+            yield return time;
+            FindVisibleElement();
+        }
+        
+    }
+
+    private void FindVisibleElement()
+    {
+        visibleTargets.Clear();
+        int cnt = Physics2D.OverlapCircle(HolderPosition, viewRadius, _enemyFilter, _enemiesInView);
+        for (int i = 0; i < cnt; i++)
+        {
+            Transform enemy = _enemiesInView[i].transform;
+            Vector3 direction = enemy.position - HolderPosition;
+
+            if (Vector3.Angle(_weaponHolder.right, direction.normalized) < viewAngle * 0.5f)
+            {
+                if (!Physics2D.Raycast(HolderPosition, direction.normalized, direction.magnitude,
+                        _obstacleMask))
+                {
+                    visibleTargets.Add(enemy);
+                }
+            }
+        }
     }
 
     private void Update()
